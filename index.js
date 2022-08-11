0d12901a0f195a484dde5e159fc0275f8b4e2f8b
Alvin <alvin.tol@hotmail.com>
2022-08-10 22:08:49 -0600

    adds post request to /shorturl

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const bodyparser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -9,16 +10,29 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', (req, res) => {
+
+  const original_url = 'https://freeCodeCamp.org';
+  const short_url = 1;
+
+  res.json({
+    original_url,
+    short_url
+  })
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
