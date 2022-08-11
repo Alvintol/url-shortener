1e58b9ac8c49ad6141556e512566f3506ae67b2c
Alvin <alvin.tol@hotmail.com>
2022-08-11 12:33:21 -0600

    adds http/www request validator to user input strings

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyparser = require('body-parser')
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-const { generateRandomString } = require('../../lighthouse/dailyTracker/Week_3/tinyApp/helpers');
 const app = express();
 
 // Basic Configuration
@@ -41,15 +40,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/:shorturl', (req, res) => {
+// app.get('/api/:shorturl', (req, res) => {
 
-  res.redirect(original_url)
-})
+//   res.redirect(original_url)
+// })
 
-const validate_url = (url) => {
-  const url_regex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+const validateUrl = (url) => {
+  const urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 
-  return url_regex.test(url);
+  return urlRegex.test(url);
 }
 
 const urlSchema = new mongoose.Schema({
@@ -59,7 +58,7 @@ const urlSchema = new mongoose.Schema({
 
 const Url = mongoose.model('Url', urlSchema);
 
-generateRandomString = () => {
+const generateRandomString = () => {
   const charList = "abcdefghijklmnopqrstuvwxyz0123456789";
   const randomID = [];
   while (randomID.length < 6) {
@@ -68,10 +67,23 @@ generateRandomString = () => {
   return randomID.join('');
 };
 
-const createAndSaveUrl = (url) => {
+const validateRequest = (url) => {
+
+  let validatedUrl = url;
+  const startsWithHttp = () => url.slice(0, 8).match(/https:\/\//g);
+  const startsWithWww = () => url.slice(0, 8).match(/www\./g);
+
+  return startsWithWww(url) ?
+    'https://' + validatedUrl :
+    startsWithHttp(validatedUrl) ?
+      validatedUrl :
+      'https://www.' + validatedUrl
+}
+
+const createAndSaveUrl = (url, shortUrl, done) => {
   const newUrl = new Url({
-    original_url: url,
-    short_url: generateRandomString()
+    original_url: validateRequest(url),
+    short_url: shortUrl
   });
 
   newUrl.save((err, url) => {
@@ -83,19 +95,19 @@ const createAndSaveUrl = (url) => {
 app.post('/api/shorturl', (req, res) => {
 
   const { url } = req.body;
-
   const original_url = url;
-  const short_url = 1;
+  const short_url = generateRandomString();
 
-  createAndSaveUrl(url);
+  if (validateRequest(url)) {
 
-  return validate_url(url) ?
+    createAndSaveUrl(url, short_url);
     res.json({
       original_url,
       short_url
     })
-    :
+  } else {
     res.json({ error: 'invalid url' })
+  }
 });
 
 app.listen(port, function () {
